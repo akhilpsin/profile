5d5b33483d0eac3ee688aed5becc63f26c961684
Akhil PS <akhilpsinbox@gmail.com>
2024-04-19 20:01:19 +0530

    Profile enhanced V1.1

---
+++ assets/js/custom.js
@@ -0,0 +1,15 @@
+VANTA.WAVES({
+    el: "#wrapper",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: true,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.000,
+    scaleMobile: 1.00,
+    color: 0x170018,
+    shininess: 82.00,
+    waveHeight: 34.00,
+    waveSpeed: 0.70,
+    zoom: 0.90
+  })
