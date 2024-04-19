d1a40b6f7f9c4923e17d0d0268642e9aceb0ac55
Akhil PS <akhilpsinbox@gmail.com>
2024-04-19 20:05:39 +0530

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,7 +7,7 @@ VANTA.WAVES({
     minWidth: 200.00,
     scale: 1.000,
     scaleMobile: 1.00,
-    color: 0x170018,
+    color: 0x121018,
     shininess: 82.00,
     waveHeight: 34.00,
     waveSpeed: 0.70,
